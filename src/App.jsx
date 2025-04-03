5c574d785674f547979c2e4e2b694fa485251e3b
ragOP <najmiraghib@gmail.com>
2025-04-03 19:32:58 +0530

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Chatbot from './Chatbot';
+import ChatbotTwo from './Chatbottwo';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/engsf1" element={<Chatbot />} />
+        <Route path="/engsf2200" element={<ChatbotTwo />} />
       </Routes>
     </Router>
   );
